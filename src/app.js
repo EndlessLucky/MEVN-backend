cc3231b7d1e15736a670c2ff6086a874a3cf96d8
JamesAlexander-rockwall <james.alexander.rockwall@gmail.com>
2020-10-30 21:57:41 +0200

    feat - implement create and read function

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+var Post = require('../models/post');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/posts');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function (callback) {
+    console.log('Connection Succeeded');
+});
 
 const app = express();
 app.use(morgan('combined'));
@@ -9,12 +17,34 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/posts', (req, res) => {
-    res.send(
-        [{
-                title: 'Hello World!',
-                description: 'Hi there! How are you?'
-            }]
-    );
+    Post.find({}, 'title description', function(error, posts){
+        if (error) {
+            console.error(error);
+        }
+        res.send({
+            posts: posts
+        })
+    }).sort({_id:-1})
 });
+//Add new post
+app.post('/posts', (req, res) =>{
+    var db = req.db;
+    var title = req.body.title;
+    var description = req.body.description;
+    var new_post = new Post({
+        title: title,
+        description: description
+    })
+
+    new_post.save(function (error){
+        if (error) {
+            console.log(error);
+        }
+        res.send({
+            success: true,
+            message: 'Post saved successfully!'
+        })
+    })
+})
 
 app.listen(process.env.PORT || 8081);
